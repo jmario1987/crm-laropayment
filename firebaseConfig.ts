febf5ce061a63f4870456e126a915c573a104cc7
Jose Mario Vasquez Jimenez <mariovasq1987@gmail.com>
2025-08-07 09:51:33 -0600

    Firebase

---
+++ firebaseConfig.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCoqFHPcxmvLS0LZyRnGcKO5XDMHvmC8f0",
+  authDomain: "crm-laropayment-app.firebaseapp.com",
+  projectId: "crm-laropayment-app",
+  storageBucket: "crm-laropayment-app.firebasestorage.app",
+  messagingSenderId: "56596263068",
+  appId: "1:56596263068:web:2b94cc8efa2ac814f6fd59"
+};
