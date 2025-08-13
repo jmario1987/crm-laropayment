5834970a40245a8ce9585b1f656e36c35578d783
Jose Mario Vasquez Jimenez <mariovasq1987@gmail.com>
2025-08-13 11:26:42 -0600

    revertir cambio pipeline

--- index.tsx
+++ index.tsx
@@ -6,13 +6,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { LeadProvider } from './context/LeadContext';
 
-// El único cambio es que hemos eliminado las etiquetas <React.StrictMode>
+// ORDEN CORREGIDO: AuthProvider envuelve a LeadProvider
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <LeadProvider>
-        <App />
-      </LeadProvider>
-    </AuthProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <LeadProvider>
+          <App />
+        </LeadProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
