7b43a097144c9c4683e23a0eaf862fbe4a498a41
Jose Mario Vasquez Jimenez <mariovasq1987@gmail.com>
2025-08-13 12:21:04 -0600

    modificacion index

--- index.tsx
+++ index.tsx
@@ -6,15 +6,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { LeadProvider } from './context/LeadContext';
 
-// ORDEN CORREGIDO: AuthProvider envuelve a LeadProvider
+// ORDEN CORREGIDO Y SIN STRICTMODE
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <LeadProvider>
-          <App />
-        </LeadProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <LeadProvider>
+        <App />
+      </LeadProvider>
+    </AuthProvider>
+  </BrowserRouter>
 );
