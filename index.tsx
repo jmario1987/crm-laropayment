2a11f71ed46254b719540dea59e6715dd2b1651a
Jose Mario Vasquez Jimenez <mariovasq1987@gmail.com>
2025-08-13 12:45:59 -0600

    index faltante

--- index.tsx
+++ index.tsx
@@ -5,8 +5,12 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { LeadProvider } from './context/LeadContext';
+import { polyfill } from 'mobile-drag-drop'; // <-- AÑADE ESTA LÍNEA
 
-// ORDEN CORREGIDO Y SIN STRICTMODE
+// Activa el "traductor" táctil
+polyfill(); // <-- AÑADE ESTA LÍNEA
+
+// Aquí ya no debería estar React.StrictMode
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <AuthProvider>
