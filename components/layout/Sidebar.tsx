ac05eecf3becdded81361d6491346aca3209d81d
JOSE MARIO VASQUEZ JIMENEZ <mariovasq1987@gmail.com>
2026-04-12 23:10:18 -0600

    reporte de placas 2

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -73,7 +73,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
             {/* --- NUEVO BOTÓN: CENTRO DE REPORTES --- */}
             <NavLink to="/reports" onClick={handleLinkClick} className={getLinkClassName}>
                <NavIcon path="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-              <span className="ml-3">Centro de Reportes</span>
+              <span className="ml-3">Reporte de Placas y Terminales</span>
             </NavLink>
             {/* --------------------------------------- */}
             
